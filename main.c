a5cc0e10c71622298e782dc513d6407893750cba
Unknown <tomsamaille@gmail.com>
2018-01-24 16:55:53 +0100

    le commit nul

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 /* Fonction principale qui lance une partie */
 int main() {
     MLV_init_audio( );
-	MLV_Music* music = MLV_load_music( "music/zelda_epique_cc.mp3" );
+	MLV_Music* music = MLV_load_music("music/zelda_epique_cc.mp3");
 	playMusic(music);
 	initialiserFenetre();
 	gererPartie();
